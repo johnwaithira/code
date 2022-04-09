87b3088ebd5d550021276802e17d0ffc0f5771ca
John Waithira <78137880+johnwaithira@users.noreply.github.com>
2022-04-09 12:20:42 +0300

    Update code.ino

--- code.ino
+++ code.ino
@@ -1,23 +1,29 @@
+#include <Servo.h>
 
-int URECHO = 3;         // PWM Output 0-50000US,Every 50US represent 1cm
-int URTRIG = 5;         // trigger pin
+Servo myservo;  
+int pos_1 = 140, pos_2 = 20, pos_start = 90;
+
+int URECHO = 3;         
+int URTRIG = 5;         
 
 unsigned int DistanceMeasured = 0;
 
 void setup()
 {
   //Serial initialization
-  Serial.begin(9600);                        // Sets the baud rate to 9600
-  pinMode(URTRIG, OUTPUT);                   // A low pull on pin COMP/TRIG
-  digitalWrite(URTRIG, HIGH);                // Set to HIGH
-  pinMode(URECHO, INPUT);                    // Sending Enable PWM mode command
+  Serial.begin(9600);                       
+  pinMode(URTRIG, OUTPUT);                   
+  digitalWrite(URTRIG, HIGH);                
+  pinMode(URECHO, INPUT);     
+  myservo.attach(11);   
+  myservo.write(90);            
   delay(500);
-  Serial.println("Init the sensor");
+  Serial.println("weCode .... ");
 
 }
 void loop()
 {
-  Serial.print("Distance=");
+  
   digitalWrite(URTRIG, LOW);
   digitalWrite(URTRIG, HIGH);              
 
@@ -28,9 +34,23 @@ void loop()
   }
   else
   {
-    DistanceMeasured = LowLevelTime / 50;  
-    Serial.print(DistanceMeasured);
-    Serial.println("cm");
+    DistanceMeasured = LowLevelTime / 50;
+    if(DistanceMeasured <=20)
+    {
+     myservo.write(pos_1);
+     delay(1000);
+     myservo.write(pos_start);
+     delay(1000);
+     myservo.write(pos_2);
+     delay(1000);
+     myservo.write(pos_start);
+     
+     
+     Serial.print("Distance=");
+     Serial.print(DistanceMeasured);
+     Serial.println("cm");   
+    }
+    
   }
 
   delay(200);
