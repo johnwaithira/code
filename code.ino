982ef2045b705b5d7031d8214724fe87b801b476
John Waithira <78137880+johnwaithira@users.noreply.github.com>
2022-04-09 11:16:13 +0300

    Create code.ino

---
+++ code.ino
@@ -0,0 +1,27 @@
+int R,C,L;
+
+{
+  int i;
+  for(i=8;i<=10;i++)
+    pinMode(i, INPUT);  
+  Serial.begin(9600);      
+  Serial.println("IR");
+}
+
+
+void loop(void)
+{
+  C = digitalRead(9);
+  R = digitalRead(8);
+  L = digitalRead(10);
+  Serial.print("Right - ");
+  Serial.println(R);
+  Serial.print("Center - ");
+  Serial.println(C);
+  Serial.print("Left - ");
+  Serial.println(L);
+  Serial.println();
+  advance(255, 255);
+
+
+}
