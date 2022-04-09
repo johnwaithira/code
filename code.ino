bd117174c65036aaa52ba4f1fb4c8818f4703f84
John Waithira <78137880+johnwaithira@users.noreply.github.com>
2022-04-09 12:06:55 +0300

    Update code.ino

--- code.ino
+++ code.ino
@@ -1,29 +1,37 @@
-//https://meet.google.com/ohx-gpni-tpw
 
-int R,C,L;
+int URECHO = 3;         // PWM Output 0-50000US,Every 50US represent 1cm
+int URTRIG = 5;         // trigger pin
+
+unsigned int DistanceMeasured = 0;
+
 void setup()
 {
-  int i;
-  for(i=8;i<=10;i++)
-    pinMode(i, INPUT);  
-  Serial.begin(9600);      
-  Serial.println("IR");
+  //Serial initialization
+  Serial.begin(9600);                        // Sets the baud rate to 9600
+  pinMode(URTRIG, OUTPUT);                   // A low pull on pin COMP/TRIG
+  digitalWrite(URTRIG, HIGH);                // Set to HIGH
+  pinMode(URECHO, INPUT);                    // Sending Enable PWM mode command
+  delay(500);
+  Serial.println("Init the sensor");
+
 }
-
-
-void loop(void)
+void loop()
 {
-  C = digitalRead(9);
-  R = digitalRead(8);
-  L = digitalRead(10);
-  Serial.print("Right - ");
-  Serial.println(R);
-  Serial.print("Center - ");
-  Serial.println(C);
-  Serial.print("Left - ");
-  Serial.println(L);
-  Serial.println();
-  
+  Serial.print("Distance=");
+  digitalWrite(URTRIG, LOW);
+  digitalWrite(URTRIG, HIGH);              
 
+  unsigned long LowLevelTime = pulseIn(URECHO, LOW) ;
+  if (LowLevelTime >= 50000)              
+  {
+    Serial.println("Invalid");
+  }
+  else
+  {
+    DistanceMeasured = LowLevelTime / 50;  
+    Serial.print(DistanceMeasured);
+    Serial.println("cm");
+  }
 
+  delay(200);
 }
